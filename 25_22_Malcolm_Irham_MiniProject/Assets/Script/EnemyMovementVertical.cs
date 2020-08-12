4b3cf1c184306a6dfc8f3760c5cc56fa0d263d99
FrozenHeavenz <64124272+FrozenHeavenz@users.noreply.github.com>
2020-08-12 09:49:06 +0800

    This is UpdatedVersion2
    Updates
    - Swapped Levels to match difficulty
    - Redesigned Level 2
    -Implemented Portal

---
+++ 25_22_Malcolm_Irham_MiniProject/Assets/Script/EnemyMovementVertical.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementVertical : MonoBehaviour
+{
+    public float MaxY;
+    public float MinY;
+
+    float direction = 0;
+
+    private float Speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == 0)
+            MoveUp();
+        else if (direction == 1)
+            MoveDown();
+    }
+
+    public void MoveUp()
+    {
+        transform.position += new Vector3(0, 1, 0) * Speed * Time.deltaTime;
+        if (transform.position.y >= MaxY)
+        {
+            direction = 1;
+        }
+    }
+
+    public void MoveDown()
+    {
+        transform.position += new Vector3(0, 1, 0) * -Speed * Time.deltaTime;
+        if (transform.position.y <= MinY)
+        {
+            direction = 0;
+        }
+    }
+}
---
+++ 25_22_Malcolm_Irham_MiniProject/Assets/Script/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public enum GameState { GameOver, GameInProgress, GamePaused };
+    public static GameState CurrentState = GameState.GamePaused;
+
+    public static GameManager thisManager;
+    public Text Txt_Message;
+    public Text ScoreText;
+    public Text LivesText;
+
+    public static float Lives = 3;
+    public static float Score = 0;
+
+    void Start()
+    {
+        thisManager = this;
+    }
+
+    void Update()
+    {
+        if (CurrentState == GameState.GamePaused)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                CurrentState = GameState.GameInProgress;
+                Txt_Message.gameObject.SetActive(false);
+            }
+        }
+        ScoreText.text = "Score : " + Score;
+        LivesText.text = "Lives : " + Lives;
+    }
+}
